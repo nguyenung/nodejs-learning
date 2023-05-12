ff46742b8b77c9549e793b28da9076078d9f0562
Ung Nguyen <ung.nguyen@saritasa.com>
2023-05-12 20:43:34 +0700

    Edit product feature

--- route/admin.js
+++ route/admin.js
@@ -4,10 +4,14 @@ const router = express.Router()
 
 const adminController = require('./../controllers/admin')
 
-router.get('/add-product', adminController.getAddProduct)
-
 router.get('/products', adminController.getProducts)
 
+router.get('/add-product', adminController.getAddProduct)
+
 router.post('/add-product', adminController.postAddProduct)
+
+router.get('/edit-product/:productId', adminController.getEditProduct)
+
+router.post('/edit-product', adminController.doEditProduct)
 
 exports.router = router
