17a3654ac2aa6b7ce527b2fbb7a630e0b5bc5f53
Ung Nguyen <ung.nguyen@saritasa.com>
2023-05-16 19:52:59 +0700

    CRUD with sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,11 +2,13 @@ const Product = require('./../models/product')
 const helpers = require('./../utils/helpers')
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
-    .then(([rows, fields]) => {
+    Product.findAll({
+        order: [['id', 'desc']]
+    })
+    .then(products => {
         res.render('admin/products', {
             path: "/admin/products",
-            products: rows,
+            products: products,
             pageTitle: "Admin - All product"
         })
     })
@@ -23,7 +25,7 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = async (req, res, next) => {
-    const newProduct = await Product.create({
+    await Product.create({
         title: req.body.title,
         imageUrl: req.body.imageUrl,
         description: req.body.description,
@@ -37,12 +39,8 @@ exports.postAddProduct = async (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
     const productId = req.params.productId
-    Product.findById(productId)
-    .then(([rows]) => {
-        const product = rows.length > 0 ? rows[0] : null;
-        if (!product) {
-            return res.redirect('/')
-        }
+    Product.findByPk(productId)
+    .then(product => {
         res.render('admin/edit-product', {
             path: "/admin/edit-product",
             pageTitle: "Edit product",
@@ -55,50 +53,35 @@ exports.getEditProduct = (req, res, next) => {
     })
 }
 
-const saveProduct = (productId, updateData) => {
-    const updatedProduct = new Product(
-        productId,
-        updateData.title,
-        updateData.imageUrl,
-        updateData.description,
-        updateData.price,
-    )
-    return updatedProduct.save()
+exports.doEditProduct = async (req, res, next) => {
+    const productId = req.body.productId
+    try {
+        const product = await Product.findByPk(productId)
+        if (product) {
+            await product.update(
+                {
+                    title: req.body.title,
+                    imageUrl: req.body.imageUrl,
+                    description: req.body.description,
+                    price: req.body.price,
+                }
+            )
+        } else {
+            throw new Error('Record not found')
+        }
+        res.redirect('/admin/products')
+    } catch(err) {
+        console.error('Error updating record:', error);
+    }
 }
 
-exports.doEditProduct = (req, res, next) => {
+exports.doDeleteProduct = async (req, res, next) => {
     const productId = req.body.productId
-
-    Product.findById(productId)
-    .then(([rows]) => {
-        const product = rows.length > 0 ? rows[0] : null;
-        if (!product) {
-            throw new Error('Product not found.');
-        }
-        return saveProduct(productId, req.body)
-    })
-    .then(() => {
-        res.redirect('/admin/products')
-    })
-    .catch(err => {
-        helpers.errorHandle(err)
-    })
-}
-
-exports.doDeleteProduct = (req, res, next) => {
-    const productId = req.body.productId
-    Product.findById(productId)
-    .then(([rows]) => {
-        const product = rows.length > 0 ? rows[0] : null;
-        if (!product) {
-            throw new Error(`Product not found: ${productId}`)
-        }
-        return Product.delete(product.id)
-    })
-    .then(() => {
-        res.redirect('/admin/products')
-    })
-    .catch(err => {
-        helpers.errorHandle(err)
-    })
+    const product = await Product.findByPk(productId)
+    if (product) {
+        await product.destroy()
+    } else {
+        throw new Error('Record not found')
+    }
+    res.redirect('/admin/products')
 }
